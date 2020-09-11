0ae49841d218cf2beba8eb64bd081fe6fcd27cb2
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-09-11 01:10:39 -0400

    jest fetch mock

--- tests/retrieveMetadata.test.js
+++ tests/retrieveMetadata.test.js
@@ -1,9 +1,18 @@
 import "@babel/polyfill";
 import retrieveMetadata from "../src/retrieveMetadata";
-global.fetch = require('node-fetch');
+import fetchMock from "jest-fetch-mock";
+fetchMock.enableMocks();
 
 describe("RetrieveMetadata", () => {
     it("Should return truthy from metadata service", async done => {
+        fetch.mockResponseOnce(JSON.stringify(
+            {
+                "@odata.context": "https://e2poc.crm9.dynamics.com/api/data/v9.0/$metadata#EntityDefinitions(LogicalName,LogicalCollectionName)", "value": [
+                    { "LogicalName": "contact", "LogicalCollectionName": "contacts", "MetadataId": "e995200c-5fe8-4843-8325-80bd2a429d1e" }
+                ]
+            }
+        ));
+
         retrieveMetadata('?$select=LogicalName,LogicalCollectionName').then(result => {
             expect(result).toBeTruthy();
             done();
