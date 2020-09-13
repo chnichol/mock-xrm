90c31a7b85d2a76e3022ccabaaf0b206429f9f83
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-09-12 23:10:35 -0400

    updated retrieve to use meta

--- src/retrieve.js
+++ src/retrieve.js
@@ -1,7 +1,10 @@
 import executeGet from "./utility/executeGet";
+import retrieveMetadataMemo from './retrieveMetadataMemo';
 
 const retrieve = async (entityLogicalName, id, options = '') => {
-    let result = await executeGet(`${entityLogicalName}s(${id})${options ? options : ''}`)();
+    let allMeta = await retrieveMetadataMemo('?$select=LogicalName,LogicalCollectionName');
+    let meta = allMeta.value.find((v) => v.LogicalName === entityLogicalName);
+    let result = await executeGet(`${meta.LogicalCollectionName}(${id})${options ? options : ''}`)();
     return result.json();
 }
 
