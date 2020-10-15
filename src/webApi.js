ec4227c5d654112a6f22e524d465e98d475881d7
Carl de Souza <carldesouza@gmail.com>
2020-10-14 21:05:38 -0400

    Added support for Xrm.WebApi.online.execute

--- src/webApi.js
+++ src/webApi.js
@@ -3,13 +3,15 @@ import create from './create';
 import update from './update';
 import del from './delete';
 import retrieveMultiple from './retrieveMultiple';
+import online from './online';
 
 const webApi = {
     createRecord: create,
     deleteRecord: del,
     updateRecord: update,
     retrieveRecord: retrieve,
-    retrieveMultipleRecords: retrieveMultiple
+    retrieveMultipleRecords: retrieveMultiple,
+    online: online
 }
 
 export default webApi;
