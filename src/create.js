619d028a9bf039dc7221c555a0ff5d7f839a3e96
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-10-20 10:54:32 -0400

    updated response data for delete, update, and create apis

--- src/create.js
+++ src/create.js
@@ -1,7 +1,12 @@
 import executePost from "./utility/executePost";
+import retrieveMetadataMemo from './retrieveMetadataMemo';
 
-const create = (entityLogicalName, data) => {
-    return executePost(data)(`/${entityLogicalName}s`)({'content-type': 'application/json'});
+const create = async (entityLogicalName, data) => {
+    let meta = await retrieveMetadataMemo(entityLogicalName);
+    let result = await executePost(data)(`/${meta.EntityMetadata.LogicalCollectionName}`)({'content-type': 'application/json', 'Prefer': 'return=representation'  });
+    let json = await result.json();
+
+    return Promise.resolve({entityType:entityLogicalName, id: json[meta.EntityMetadata.PrimaryIdAttribute]});
 }
 
 export default create;
--- src/delete.js
+++ src/delete.js
@@ -1,7 +1,11 @@
 import executeDelete from "./utility/executeDelete";
+import retrieveMetadataMemo from './retrieveMetadataMemo';
 
-const del = (entityLogicalName, id) => {
-    return executeDelete(`/${entityLogicalName}s(${id})`)();
+const del = async (entityLogicalName, id) => {
+    let meta = await retrieveMetadataMemo(entityLogicalName);
+    let result = await executeDelete(`/${meta.EntityMetadata.LogicalCollectionName}(${id})`)();
+
+    return Promise.resolve({entityType: entityLogicalName, id: id});
 }
 
 export default del;
