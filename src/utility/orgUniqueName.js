2aa54549ccc9b7a630e1ea9baa18102817bc9eb0
carldesouzacom <62360636+carldesouzacom@users.noreply.github.com>
2020-12-16 14:48:26 -0500

    Updated to account for orgs with dashes

--- src/utility/orgUniqueName.js
+++ src/utility/orgUniqueName.js
@@ -2,7 +2,7 @@ import memoize from './memoize';
 
 const orgUniqueName = memoize(async () => {
     let result = await fetch('/tools/systemcustomization/systemCustomization.aspx?pid=05&web=true');
-    return result.text().then(data => data.match(/var ORG_UNIQUE_NAME = '([a-zA-Z0-9]*)';/)[1]);
+    return result.text().then(data => data.match(/var ORG_UNIQUE_NAME = '([-a-zA-Z0-9]*)';/)[1]);
 });
 
 export default orgUniqueName;
